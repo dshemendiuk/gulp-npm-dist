8db43719b607909584f8b6708e23098c9a75358e
Dima Sh <shimdim@gmail.com>
2017-06-11 13:09:01 +0300

    Delete unminified versions of libs

--- index.js
+++ index.js
@@ -1,49 +1,83 @@
 var fs = require('fs');
 
 module.exports = function() {
-  var buffer, packageJson, packages;
-  
-  buffer = fs.readFileSync('./package.json');
-  packageJson = JSON.parse(buffer.toString());
-  packages = [];
-  
-  for (package in packageJson.dependencies) {
-    var depPackageBuffer = fs.readFileSync('./node_modules/' + package + '/package.json');
+  var buffer = fs.readFileSync('./package.json');
+  var packageJson = JSON.parse(buffer.toString());
+  var packages = [];
+
+  for (lib in packageJson.dependencies) {
+    var depPackageBuffer = fs.readFileSync('./node_modules/' + lib + '/package.json');
     var depPackage = JSON.parse(depPackageBuffer.toString());
+    var mainFileFolder = './node_modules/' + lib;
+    var libFiles = [];
 
     if (depPackage.main) {
-      var mainFile = './node_modules/' + package + '/' + depPackage.main;
-      var mainFileFolder = mainFile.slice(0, mainFile.lastIndexOf('/'));
-      var distFolderPos = mainFile.lastIndexOf('/dist'); 
+      var mainFile = mainFileFolder + '/' + depPackage.main;
+      var distFolderPos;
+
+      distFolderPos = mainFile.lastIndexOf('/dist');
+      mainFileFolder = mainFile.slice(0, mainFile.lastIndexOf('/'));
 
       if (distFolderPos !== -1) {
-        mainFileFolder = mainFile.substring(0, distFolderPos) + '/dist'; 
+        mainFileFolder = mainFile.substring(0, distFolderPos) + '/dist';
       }
 
-      packages.push('!' + mainFileFolder + '/**/*.map');
-      packages.push('!' + mainFileFolder + '/src/**/*');
-      packages.push('!' + mainFileFolder + '/examples/**/*');
-      packages.push('!' + mainFileFolder + '/example/**/*');
-      packages.push('!' + mainFileFolder + '/demo/**/*');
-      packages.push('!' + mainFileFolder + '/spec/**/*');
-      packages.push('!' + mainFileFolder + '/docs/**/*');
-      packages.push('!' + mainFileFolder + '/tests/**/*');
-      packages.push('!' + mainFileFolder + '/test/**/*');
-      packages.push('!' + mainFileFolder + '/**/Gruntfile.js');
-      packages.push('!' + mainFileFolder + '/**/gulpfile.js');
-      packages.push('!' + mainFileFolder + '/**/package.json');
-      packages.push('!' + mainFileFolder + '/**/bower.json');
-      packages.push('!' + mainFileFolder + '/**/*.md');
-      packages.push('!' + mainFileFolder + '/**/*.coffee');
-      packages.push('!' + mainFileFolder + '/**/*.ts');
-      packages.push('!' + mainFileFolder + '/**/*.scss');
-      packages.push('!' + mainFileFolder + '/**/*.less');
-
-      packages.push(mainFileFolder + '/**/*.*');
     } else {
-      console.log('Main file is not defined for the module ' + package);
-      packages.push('./node_modules/' + package + '/**/*');
-    } 
+      console.log('Main file is not defined for the module ' + lib);
+    }
+
+    //delete unminified versions
+
+    function readLibFilesRecursively(target) {
+      try {
+        fs.readdirSync(target).forEach(function(path) {
+          var fullPath = target + '/' + path;
+          if (fs.lstatSync(fullPath).isDirectory()) {
+            readLibFilesRecursively(fullPath);
+          }
+          libFiles.push(fullPath);
+        });
+      } catch(err) {
+        console.log(err);
+      }
+    }
+
+    readLibFilesRecursively(mainFileFolder);
+
+    for (var i=0; i<libFiles.length; i++) {
+      var target;
+      if (libFiles[i].indexOf('.min.js') > -1) {
+        target = libFiles[i].replace(/\.min\.js/, '.js');
+        packages.push('!' + libFiles[libFiles.indexOf(target)]);
+      }
+      if (libFiles[i].indexOf('.min.css') > -1) {
+        target = libFiles[i].replace(/\.min\.css/, '.css');
+        packages.push('!' + libFiles[libFiles.indexOf(target)]);
+      }
+    }
+
+    packages.push('!' + mainFileFolder + '/**/*.map');
+    packages.push('!' + mainFileFolder + '/src/**/*');
+    packages.push('!' + mainFileFolder + '/examples/**/*');
+    packages.push('!' + mainFileFolder + '/example/**/*');
+    packages.push('!' + mainFileFolder + '/demo/**/*');
+    packages.push('!' + mainFileFolder + '/spec/**/*');
+    packages.push('!' + mainFileFolder + '/docs/**/*');
+    packages.push('!' + mainFileFolder + '/tests/**/*');
+    packages.push('!' + mainFileFolder + '/test/**/*');
+    packages.push('!' + mainFileFolder + '/**/Gruntfile.js');
+    packages.push('!' + mainFileFolder + '/**/gulpfile.js');
+    packages.push('!' + mainFileFolder + '/**/package.json');
+    packages.push('!' + mainFileFolder + '/**/bower.json');
+    packages.push('!' + mainFileFolder + '/**/composer.json');
+    packages.push('!' + mainFileFolder + '/**/*.md');
+    packages.push('!' + mainFileFolder + '/**/*.coffee');
+    packages.push('!' + mainFileFolder + '/**/*.ts');
+    packages.push('!' + mainFileFolder + '/**/*.scss');
+    packages.push('!' + mainFileFolder + '/**/*.less');
+
+    packages.push(mainFileFolder + '/**/*');
+
   }
 
   return packages;
